fbfbc7d3c5d5823c5a959fcc1bbac38090b713c0
qianqiancheng <you@example.com1282825578@qq.com>
2022-08-22 11:16:10 +0800

    请求封装

--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.less'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 // 创建vue 根实例
 // 把router 配置到根实例中
 // 通过render方法把 APP根组件渲染到 #app入口节点
